073e9d2b9373bcc21fea8fae8747ecd2c6743af9
Joey <jpapu@unither.com>
2021-10-04 16:34:55 -0400

    add some datetimes to post object

--- portfolio/web_app/models.py
+++ portfolio/web_app/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     body = models.TextField()
 
     def __str__(self):
