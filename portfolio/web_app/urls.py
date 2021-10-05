90be20e12f4e3dcf280f26fbfc972b6170d4b770
Joey <jpapu@unither.com>
2021-10-05 12:43:57 -0400

    make post linkable

--- portfolio/web_app/urls.py
+++ portfolio/web_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from web_app.views import HomeView
+from web_app.views import HomeView, ArticleDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- portfolio/web_app/views.py
+++ portfolio/web_app/views.py
@@ -7,3 +7,7 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
