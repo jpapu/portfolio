5b505cfe5b6e435920ca0ab34061582c17688916
Joey <jpapu@unither.com>
2021-10-05 06:59:00 -0400

    list posts in for loop

--- portfolio/web_app/views.py
+++ portfolio/web_app/views.py
@@ -2,6 +2,7 @@ from typing import List
 from django.shortcuts import render
 from django.view.generic import ListView, DetailView
 from portfolio.web_app.models import Post
+from .models import Post
 
 class HomeView(ListView):
     model = Post
