501b8de29f668b4b5edfdcec94b965086a5d9cfb
Joseph Papu <36134898+jpapu@users.noreply.github.com>
2021-10-05 13:24:12 -0400

    10 link posts (#11)
    * alter views
    
    * list posts in for loop
    
    * fix urls
    
    * WIP
    
    * bug fixes
    
    * make post linkable
    
    * fix typos

--- portfolio/web_app/urls.py
+++ portfolio/web_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from web_app.views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- portfolio/web_app/views.py
+++ portfolio/web_app/views.py
@@ -1,4 +1,13 @@
+from typing import List
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def index(request):
-    return render(request, 'index.html', {})
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
