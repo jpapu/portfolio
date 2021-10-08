765ceb4443826276cfd366d386380d2803325b24
Joseph Papu <36134898+jpapu@users.noreply.github.com>
2021-10-08 15:42:57 -0400

    14 add post page (#15)
    * migrate
    
    * create post page
    
    * make forms pretty

--- portfolio/web_app/urls.py
+++ portfolio/web_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from web_app.views import HomeView, ArticleDetailView
+from web_app.views import CreatePostView, HomeView, ArticleDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('create_post/', CreatePostView.as_view(), name='create_post'),
 ]
--- portfolio/web_app/views.py
+++ portfolio/web_app/views.py
@@ -1,8 +1,9 @@
 from typing import List
 from django.shortcuts import render
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from.forms import CreatePostForm
 
 class HomeView(ListView):
     model = Post
@@ -11,3 +12,10 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = CreatePostForm
+    template_name = 'create_post.html'
+    #fields = '__all__'
+    #fields = ('title', 'body')
