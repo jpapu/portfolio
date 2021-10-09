2f0543265465781df6b21267c1bd69bc30d66f48
Joey <jpapu@unither.com>
2021-10-09 06:59:42 -0400

    finish out editing

--- portfolio/web_app/urls.py
+++ portfolio/web_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from web_app.views import CreatePostView, HomeView, ArticleDetailView
+from web_app.views import CreatePostView, HomeView, ArticleDetailView, UpdatePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('create_post/', CreatePostView.as_view(), name='create_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
 ]
