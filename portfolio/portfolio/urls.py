2e9426f75b6c7e7b4c7772a6b3e38c3b5f016815
Joey <jpapu@unither.com>
2021-10-10 06:52:09 -0400

    begining of user credetials

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web_app.urls')),
+    path('auth_app/', include('django.contrib.auth.urls')),
+    path('auth_app/', include('auth_app.urls')),
 ]
