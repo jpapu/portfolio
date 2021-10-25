f514c5637f50e2264a9c8488929b1bb52b20babc
jpapu <jpapu@unither.com>
2021-10-24 20:30:15 -0400

    create pretty page

--- portfolio/auth_app/views.py
+++ portfolio/auth_app/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm, EditProfileForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
@@ -16,3 +17,10 @@ class UserEditView(generic.UpdateView):
 
     def get_object(self):
         return self.request.user
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    success_url = reverse_lazy('password_success')
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
