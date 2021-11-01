d7ac4333a1540cabe2bfbb0b201d6aa5f8a9dc97
jpapu <jpapu@unither.com>
2021-11-01 06:11:33 -0400

    work in profile editing

--- portfolio/auth_app/forms.py
+++ portfolio/auth_app/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms.widgets import TextInput
+from web_app.models import UserProfile
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
@@ -17,7 +19,7 @@ class SignUpForm(UserCreationForm):
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
 
-class EditProfileForm(UserChangeForm):
+class EditSettingsForm(UserChangeForm):
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -36,3 +38,25 @@ class PasswordChangingForm(PasswordChangeForm):
     class Meta:
         model = User
         fields = ['old_password', 'new_password1', 'new_password2']
+
+class ProfilePageForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'profile_pic', 'git_url', 'lnkdin_url']
+        widgets = {
+            'bio': forms.Textarea(attrs={'class':'form-control'}),
+            #'profile_pic':forms.,
+            'git_url': forms.TextInput(attrs={'class':'form-control'}),
+            'lnkdin_url': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'git_url', 'lnkdin_url']
+        widgets = {
+            'bio': forms.Textarea(attrs={'class':'form-control'}),
+            #'profile_pic':forms.,
+            'git_url': forms.TextInput(attrs={'class':'form-control'}),
+            'lnkdin_url': forms.TextInput(attrs={'class':'form-control'}),
+        }
--- portfolio/web_app/models.py
+++ portfolio/web_app/models.py
@@ -45,3 +45,6 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+    def get_absolute_url(self):
+        return reverse('home')  #return to home after creation
